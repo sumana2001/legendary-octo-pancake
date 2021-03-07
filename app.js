8786e6365f380a7e2dc60e6bca75c2a694100290
sumana2001 <sumana.basu2001@gmail.com>
2021-03-07 19:20:59 +0530

    Routing

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv/config');
+
+const postsRoute = require('./routes/posts');
+app.use('/posts',postsRoute);
 
 app.get('/',(req,res)=>{
     res.send("It's working!");
-})
+});
+
+
+//Connect to db
+mongoose.connect(process.env.DB_CONNECTION,
+{ useNewUrlParser: true ,
+useUnifiedTopology: true},
+()=>
+console.log("connected to db!")
+);
 
 app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.send("Posts!!");
+});
+
+router.get('/specific',(req,res)=>{
+    res.send("Specific Posts!!");
+});
+
+module.exports = router;
